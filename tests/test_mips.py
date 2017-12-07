54c4d2ce32ea649eaa609fe0c72cf603c82419a7
Jack Evans <jack@evans.gb.net>
2017-12-07 12:22:27 +0000

    Added tests for mips output

--- tests/test_mips.py
+++ tests/test_mips.py
@@ -1,15 +1,33 @@
-from glob import glob
+import re
 from compiler.compile import make_ast
+from compiler.mips import build_mips
 from compiler.parse import parse_ast
 from compiler.tac import build_tac
-from compiler.mips import build_mips
+from glob import glob
+from subprocess import PIPE, run
+from tempfile import NamedTemporaryFile
+
+HEADER_REGEX = re.compile(r"\/\*\s*(Answer|Error):\s*(.*)\s*\*\/")
 
 
-# def test_examples():
-#     for src in glob('examples/*.cmm'):
-#         with open(src) as src_file:
-#             head = parse_ast(make_ast(src_file))
-#             tac_list = []
-#             build_tac(head, tac_list)
-#             mips = build_mips(tac_list)
-#             # [TODO] Actually call mips
+def file_to_mips(f):
+    return
+
+
+def test_examples():
+    for src in glob('examples/*.cmm'):
+        with open(src) as src_file:
+            head = src_file.readline().strip()
+            match = HEADER_REGEX.match(head)
+            if match:
+                src_file.seek(0)
+                category, expected = match.groups()
+                mips = build_mips(build_tac(parse_ast(make_ast(src_file))))
+
+                with NamedTemporaryFile() as temp:
+                    temp.write("\n".join(mips).encode('utf-8'))
+                    temp.seek(0)
+                    cmd = f"spim load {temp.name} | tail -n +6"
+                    out = run(cmd, shell=True, stdout=PIPE)
+                    output = out.stdout.decode('utf-8').strip()
+                    assert output == expected.strip()
